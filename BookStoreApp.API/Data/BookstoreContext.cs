fc2558a4b378ae7fdb1f0664a7409c344d7e6285
Thiago Leite <tmedeirosleite@axon.com>
2024-01-10 21:52:51 -0300

    Added database

---
+++ BookStoreApp.API/Data/BookstoreContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.API.Data;
+
+public partial class BookstoreContext : DbContext
+{
+    public BookstoreContext()
+    {
+    }
+
+    public BookstoreContext(DbContextOptions<BookstoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Bookstore;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.Property(e => e.FirstName).HasMaxLength(60);
+            entity.Property(e => e.LastName).HasMaxLength(60);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.Property(e => e.Isbn).HasMaxLength(20);
+            entity.Property(e => e.Summary).HasMaxLength(60);
+            entity.Property(e => e.Title).HasMaxLength(30);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books).HasForeignKey(d => d.AuthorId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
