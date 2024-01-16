be489860baa718c2a29747e9e500bfa0578e81d3
Thiago Leite <tmedeirosleite@axon.com>
2024-01-15 22:41:39 -0300

    Added authentication to controllers

--- BookStoreApp.API/Controllers/BooksController.cs
+++ BookStoreApp.API/Controllers/BooksController.cs
@@ -9,11 +9,13 @@ using BookStoreApp.API.Data;
 using AutoMapper;
 using BookStoreApp.API.Models.Book;
 using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BookStoreApp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BooksController : ControllerBase
     {
         private readonly BookstoreContext _context;
@@ -88,6 +90,8 @@ namespace BookStoreApp.API.Controllers
         // PUT: api/Books/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [Authorize(Roles = "Administrator")]
+
         public async Task<IActionResult> PutBook(int id, BookUpdateDto bookDto)
         {
             if (id != bookDto.Id)
@@ -130,6 +134,8 @@ namespace BookStoreApp.API.Controllers
         // POST: api/Books
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [Authorize(Roles = "Administrator")]
+
         public async Task<ActionResult<Book>> PostBook(BookCreateDto bookDto)
         {
             try
@@ -155,6 +161,8 @@ namespace BookStoreApp.API.Controllers
 
         // DELETE: api/Books/5
         [HttpDelete("{id}")]
+        [Authorize(Roles = "Administrator")]
+
         public async Task<IActionResult> DeleteBook(int id)
         {
             try
